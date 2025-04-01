7e066dfd7122246be8407286985865d8b981f2e0
Tre <tresoldi.francesco@studenti.salesianisesto.it>
2025-04-01 13:39:49 +0200

    Create BandMember.java

---
+++ Java/Thread/Esercizi/EserciziIntroduttiviThread/ConcertJamSession/BandMember.java
@@ -0,0 +1,27 @@
+public class BandMember extends Thread {
+    private String suono;
+    private int delay;
+    private boolean isPlaying;
+
+    public BandMember(String suono, int delay) {
+        this.suono = suono;
+        this.delay = delay;
+    }
+
+    @Override
+    public void run() {
+        isPlaying = true;
+        while (isPlaying) {
+            System.out.println(suono);
+            try {
+                sleep(delay);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public void stopPlaying() {
+        isPlaying = false;
+    }
+}
