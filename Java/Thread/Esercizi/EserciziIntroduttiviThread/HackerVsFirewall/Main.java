fb9cdd65d0e1b3a473232581072a29e166b834bc
Tre <tresoldi.francesco@studenti.salesianisesto.it>
2025-04-01 13:36:37 +0200

    Create Main.java

---
+++ Java/Thread/Esercizi/EserciziIntroduttiviThread/HackerVsFirewall/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Hacker hacker = new Hacker("Kevin Mitnick");
+        Firewall firewall = new Firewall("Firewall L1", hacker);
+
+        hacker.start();
+        firewall.start();
+    }
+}
