2f18716544ff3262d355aeb1bc321e8dbf53b490
Tre <tresoldi.francesco@studenti.salesianisesto.it>
2025-04-08 12:23:06 +0200

    Create Main.java

---
+++ Java/Thread/Esercizi/EserciziIntroduttiviThread/GaraThread/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Runner usain = new Runner("Usain Bolt");
+        Runner justin = new Runner("Justin Gatlin");
+        Runner marcel = new Runner("Marcel Jacobs");
+
+        usain.start();
+        justin.start();
+        marcel.start();
+    }
+}
