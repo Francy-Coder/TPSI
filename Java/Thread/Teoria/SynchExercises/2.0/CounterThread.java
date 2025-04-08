6ad95f812342c7338db99105860d03e6f1288828
Tre <tresoldi.francesco@studenti.salesianisesto.it>
2025-04-08 13:37:53 +0200

    Update CounterThread.java

--- Java/Thread/Teoria/SynchExercises/2.0/CounterThread.java
+++ Java/Thread/Teoria/SynchExercises/2.0/CounterThread.java
@@ -1,14 +1,32 @@
 public class CounterThread extends Thread {
     private Counter counter;
+    private String operation;
 
-    public CounterThread(Counter counter) {
+    public CounterThread(Counter counter, String operation) {
         this.counter = counter;
+        this.operation = operation;
     }
 
     @Override
     public void run() {
-        for (int i = 0; i < 10000; i++) {
-            counter.add(1);
+        switch (operation) {
+            case "add":
+                for (int i = 0; i < 10000; i++) {
+                    counter.add(1);
+                }
+                break;
+            case "sub":
+                for (int i = 0; i < 10000; i++) {
+                    counter.sub(1);
+                }
+                break;
+            case "mult":
+                for (int i = 0; i < 2; i++) {
+                    counter.mult(2);
+                }
+                break;
+            default:
+                throw new IllegalArgumentException("Unsupported operation: " + operation);
         }
     }
 }
