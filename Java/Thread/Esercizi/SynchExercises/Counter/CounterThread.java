6f12bad5de17941ecf2e504c751dfda23d134f14
Tre <tresoldi.francesco@studenti.salesianisesto.it>
2025-04-08 12:44:05 +0200

    Update CounterThread.java

--- Java/Thread/Esercizi/SynchExercises/Counter/CounterThread.java
+++ Java/Thread/Esercizi/SynchExercises/Counter/CounterThread.java
@@ -1,4 +1,6 @@
-public class CounterThread implements Runnable {
+public class CounterThread extends Thread {
+    //E' meglio estendere Thread, visto che se si implementa la classe runnable, si puo solo usare il metodo run
+
     //Ogni istanza di questo thread deve sommare 1 al contatore (Unico) x 10k volte
 
     private final Counter counter;
