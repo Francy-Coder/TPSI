b8877239ac18e98e8bee878a70733999cd33c979
Tre <tresoldi.francesco@studenti.salesianisesto.it>
2025-04-08 13:36:31 +0200

    Update CounterThread.java

--- Java/Thread/Teoria/SynchExercises/2.0/CounterThread.java
+++ Java/Thread/Teoria/SynchExercises/2.0/CounterThread.java
@@ -1,34 +1,14 @@
 public class CounterThread extends Thread {
-    public enum Operation {
-        ADD, SUB, MULT
-    }
-
     private Counter counter;
-    private Operation operation;
 
-    public CounterThread(Counter counter, Operation operation) {
+    public CounterThread(Counter counter) {
         this.counter = counter;
-        this.operation = operation;
     }
 
     @Override
     public void run() {
-        switch (operation) {
-            case ADD:
-                for (int i = 0; i < 10000; i++) {
-                    counter.add(1);
-                }
-                break;
-            case SUB:
-                for (int i = 0; i < 10000; i++) {
-                    counter.sub(1);
-                }
-                break;
-            case MULT:
-                for (int i = 0; i < 2; i++) {
-                    counter.mult(2);
-                }
-                break;
+        for (int i = 0; i < 10000; i++) {
+            counter.add(1);
         }
     }
 }
