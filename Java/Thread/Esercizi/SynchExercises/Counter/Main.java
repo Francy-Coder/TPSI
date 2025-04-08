e1feb01989817b936c1d5825a51bb3349f4e9cdf
Tre <tresoldi.francesco@studenti.salesianisesto.it>
2025-04-08 12:55:31 +0200

    Update Main.java

--- Java/Thread/Esercizi/SynchExercises/Counter/Main.java
+++ Java/Thread/Esercizi/SynchExercises/Counter/Main.java
@@ -5,15 +5,25 @@ public class Main {
     public static void main(String[] args) {
         //Creare N Thread che vanno a lavorare sul
         Counter c = new Counter();
-        
+
         ArrayList<Thread> threads = new ArrayList<>();
-        
+
         //Creo N threads
         for(int i = 0; i < 4; i++){
             threads.add(new CounterThread(c));
         }
-        
+
         //Li faccio partire
         for (Thread t : threads) t.start();
+
+        for (Thread t : threads){
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + "Interrotto"); //Thread.currentThread() restituisce il thread che stai eseguendo
+            }
+        }
+
+        System.out.println(c.getValue());
     }
 }
