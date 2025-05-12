d4e01a4946742da5fbb3892efc8a76f9cf44d9cb
Tre <tresoldi.francesco@studenti.salesianisesto.it>
2025-05-12 22:59:16 +0200

    Update TicketManager.java

--- Java/Thread/Esercizi/EserciziSincronizzazioneThread/BigliettiOnline/TicketManager.java
+++ Java/Thread/Esercizi/EserciziSincronizzazioneThread/BigliettiOnline/TicketManager.java
@@ -29,24 +29,3 @@ class TicketManager {
         return availableTickets.get();
     }
 }
-
-
-
-public class OnlineTicketSystem {
-    public static void main(String[] args) {
-        final int TOTAL_TICKETS = 10;
-        final int TOTAL_BUYERS = 20;
-
-        TicketManager ticketManager = new TicketManager(TOTAL_TICKETS);
-        
-        System.out.println("Sistema di vendita biglietti online");
-        System.out.println("Biglietti disponibili: " + ticketManager.getAvailableTickets());
-        System.out.println("Numero di acquirenti: " + TOTAL_BUYERS);
-        System.out.println("-----------------------------------");
-
-        // Creazione e avvio degli acquirenti
-        for (int i = 1; i <= TOTAL_BUYERS; i++) {
-            new Buyer("Acquirente-" + i, ticketManager).start();
-        }
-    }
-}
