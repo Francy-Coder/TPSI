4356c30e7d40825f085a020654e96044999d9a8b
Tre <tresoldi.francesco@studenti.salesianisesto.it>
2025-09-11 12:21:23 +0200

    Update Main.java

--- CompitiVacanze/Multithreading/Synchronized/MagazzinoConIspettore/CodiceProfessore/Main.java
+++ CompitiVacanze/Multithreading/Synchronized/MagazzinoConIspettore/CodiceProfessore/Main.java
@@ -1,19 +1,38 @@
-package CompitiVacanze.MagazzinoConIspettore;
-
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Produttore> produttori = new ArrayList<>();
+        final int capacity = 5;
+        final int numProducers = 10; //assicurarsi che numProducers sia strettamente maggiore di capacity
+        final int sogliaIspettore = 50;
 
-        Magazzino magazzino = new Magazzino(10);
+        Magazzino magazzino = new Magazzino(capacity, sogliaIspettore);
 
-        for (int i = 0; i < 20; i++){
-            produttori.add(new Produttore(magazzino));
+        // start producers
+        ArrayList<Producer> producers = new ArrayList<>();
+        for (int i = 0; i < numProducers; i++) producers.add(new Producer(i, magazzino));
+        for (Producer p : producers) p.start();
+
+        // un consumatore just for fun, si può e deve testare con più consumatori.
+        Thread consumer = new Consumer(1, magazzino);
+        consumer.start();
+
+        // Ispettore
+        Thread inspector = new Inspector(magazzino, 3000);
+        inspector.setDaemon(true); // Piccolo trick non strettamente necessario ;)
+        inspector.start();
+
+        // Il programma sarebbe tendenzialmente infinito... ci metto un limite di tempo così da chiudere in automatico.
+        try {
+            Thread.sleep(12000);
+        } catch (InterruptedException ignored) {
         }
 
-        for(Produttore p : produttori){
-            p.start();
-        }
+        for (Thread t : producers) t.interrupt();
+        consumer.interrupt();
+        inspector.interrupt();
+
+        System.out.println("Main exiting.");
     }
 }
+
