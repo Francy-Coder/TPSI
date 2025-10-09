4619c197ae249d55901df9f7ce608d230d181068
Francy <tresoldi.francesco@studenti.salesianisesto.it>
2025-10-09 13:38:44 +0200

    Create typeCheck.js

---
+++ HTML/Esercizi/TypeCheckJS/typeCheck.js
@@ -0,0 +1,60 @@
+let a = "5";
+let b = 2;
+let arr2 = [1, 2, "3", [4, 5]];
+
+console.log(a - b);    
+console.log(a + b);       
+console.log("5" * "2");     
+console.log("5" * "ciao");  
+console.log(true + false);    
+console.log(true + 10);       
+console.log(false - 1);       
+console.log(typeof Infinity);
+console.log(typeof arr2);
+console.log(typeof arr2[3]);
+console.log(1 + 2 + "3" + 4);        
+console.log("1" - "2" + 3);          
+console.log("1" + 2 - 3);            
+console.log(true + "true" + 1);      
+console.log(false + "false");       
+console.log(typeof new Number(5));
+console.log(typeof new String("ciao"));
+typeof (typeof 123)
+console.log(Infinity + 1);
+console.log(Infinity - Infinity); 
+console.log(typeof ((true + false) * 2));  
+console.log(typeof (!(false || true)));     
+console.log(typeof (!!(0 && "ciao")));     
+console.log(typeof (true + " is true"));    
+console.log(typeof ((1 + "2") * (3 + "4")));   
+console.log(typeof ((true + 2) + ("3" + false))); 
+
+/*
+Valore e il relativo tipo delle espressioni:
+1. 3, number
+2. 52, string
+3. 10, number
+4. NaN, number
+5. 1, number
+6. 11, number
+7. -1, number
+8. number
+9. object
+10. object
+11. 334, string
+12. 2, number
+13. 9, number
+14. truetrue1, string
+15. falsefalse, string
+16. object
+17. object
+18.
+19. Infinity, number
+20. NaN
+21. number
+22. boolean
+23. boolean
+24. string
+25. number
+26. string
+*/
