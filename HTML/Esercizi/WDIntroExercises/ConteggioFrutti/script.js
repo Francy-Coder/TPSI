88e790e1e95442fac265ea819e87bb9634438a2d
Francy <tresoldi.francesco@studenti.salesianisesto.it>
2025-10-26 22:56:03 +0100

    Update script.js

---
+++ HTML/Esercizi/WDIntroExercises/ConteggioFrutti/script.js
@@ -0,0 +1,6 @@
+const frutti = ["Mela", "Banana", "Pera"];
+
+function mostraLunghezza() {
+  document.getElementById("output").textContent =
+    "L'array contiene " + frutti.length + " frutti.";
+}
