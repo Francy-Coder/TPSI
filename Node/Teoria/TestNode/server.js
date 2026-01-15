003b3a3d3f5bfe0a31afc149c57f5b5dbd0443fd
Francy <187552164+Francy-Coder@users.noreply.github.com>
2026-01-15 13:36:55 +0100

    Update server.js

--- Node/Teoria/TestNode/server.js
+++ Node/Teoria/TestNode/server.js
@@ -3,9 +3,34 @@ const http = require("http")
 const PORT = 1025;
 const HOSTNAME = "localhost";
 
+let users = [
+    {username:"UNAME1", age:17},
+    {username:"UNAME2", age:18},
+    {username:"UNAME3", age:19}
+]
+
 // Per creare un server su node, possiamo utilizzare:
 const server = http.createServer((req, res) => {
-    res.end("Ciao, benvenuto");
+
+    if(req.url === "/"){
+        res.statusCode = 200; // OK
+        res.setHeader("Content-Type", "text/plain");
+        res.end("Ciao, benvenuto"); //Manda la risposta
+
+    } if (req.url === "/users" && req.method === "GET") {
+        res.statusCode = 200; 
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(users));
+
+    } else if (req.url === "/users" && req.method === "POST") {
+        res.statusCode = 405; //Methond Not Allowed
+        res.end();
+    } else {
+        res.statusCode = 404; //Not Found
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify({error:"not found"}));
+    }
+
 }) //request = richiesta che arriva dal client || response = risposta che mando al client
 
 server.listen(PORT, HOSTNAME, () => {
@@ -20,8 +45,8 @@ cntrl + C <-- Ferma esecuzione terminale
 
 * Sul Server:
 node server.js
+node --watch server.js
 
 * Sul Client:
 curl localhost:1025 <-- Client URL, un programmino che permette di eseguire chiamate di rete livello x (dipende dal protocollo), la default di curl è GET
-
 */
