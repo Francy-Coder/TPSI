98ce1c5cb90e98d3f3820ce8f728fcc4f4634568
Francy <187552164+Francy-Coder@users.noreply.github.com>
2026-02-12 13:44:42 +0100

    Update and rename vuoto.txt to spaceStationData.js

--- Node.js/Esercizi/SpaceStationIndex/2.0/spaceStationData.js
+++ Node.js/Esercizi/SpaceStationIndex/2.0/spaceStationData.js
@@ -1,3 +1,46 @@
+const BASE_URL = "http://localhost:3000";
+
+async function getPanels() {
+    const response = await fetch(BASE_URL + "/station/status");
+
+    if (response.ok) {
+        const json = await response.json()
+        // console.log(json);
+        const panels = json.power.solar.panels
+        const totalPanels = panels.length
+        // console.log(panels);
+
+        //Trova solo quelli operativi
+        let activePanels = 0;
+        for (const panel of panels) {
+            if (panel.status === "nominal") {
+                activePanels++;
+            }
+        }
+
+        const percentage = (activePanels / totalPanels) * 100;
+
+        console.log(json);
+        console.log(panels);
+
+        return {
+            totalPanels: totalPanels,
+            operationalPanels: activePanels,
+            percentage: percentage
+        }
+    }else{
+        console.log("Errore HTTP:" + response.status);
+    }
+}
+
+async function printResults() {
+    const results = await getPanels();
+    //console.clear();
+    console.log(results);
+}
+
+printResults();
+
 async function getDegradedModules() {
     const response = await fetch(BASE_URL + "/station/modules");
     if (!response.ok) return;
@@ -79,5 +122,4 @@ async function checkEnergyEmergency() {
         return { message: "Energia sufficiente, nessuna azione necessaria" };
     }
 }
-
-
+}
